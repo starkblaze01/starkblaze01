b88a98cc654c1a451e9a8284952d5d41bd675b3b
starkblaze01 <mp.pathela@gmail.com>
2019-07-23 00:26:07 +0530

    few changes

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import injectSheet from "react-jss";
 import Particle from "./Particles";
 import { Typography } from "antd";
+// import { useTransition, animated } from "react-spring";
 
 const styles = (theme: any) => ({
   align: {
@@ -16,7 +17,25 @@ const styles = (theme: any) => ({
   }
 });
 const { Title } = Typography;
+
+// const Animate: React.SFC<any> = (props: any) => {
+//   const [items, set] = React.useState(["1", "2", "3", "4"]);
+//   const transitions = useTransition(items, item => item, {
+//     from: { transform: "translate3d(0,-40px,0)" },
+//     enter: { transform: "translate3d(0,0px,0)" },
+//     leave: { transform: "translate3d(0,-40px,0)" }
+//   });
+//   return transitions.map(({ item, props, key }) => (
+//     <animated.div key={key} style={props}>
+//       {item}
+//     </animated.div>
+//   ));
+// };
+
 class Introduction extends React.PureComponent<any, any> {
+  state: any = {
+    count: 0
+  };
   render() {
     const { classes } = this.props;
     return (
@@ -24,6 +43,8 @@ class Introduction extends React.PureComponent<any, any> {
         <Particle />
         <div className={classes.align}>
           <Title>Hi!</Title>
+          <h1>I am Mayank Pathela!</h1>
+          {/* <Animate /> */}
         </div>
       </>
     );
