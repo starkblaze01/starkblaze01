d60cd0874e8d6e108d53f9d6fe46c8b3e34333e0
Mayank Pathela <mp.pathela@gmail.com>
2019-08-17 22:03:27 +0530

    Add Projects

--- src/reducers/gitrepoReducer.ts
+++ src/reducers/gitrepoReducer.ts
@@ -1,5 +1,11 @@
-import { GET_AI_CODES, GET_JENERETA, GET_SENTIMENT_ANALYZER } from '../actions/constants';
+import {
+    GET_AI_CODES, GET_JENERETA, GET_SENTIMENT_ANALYZER, ENABLE_AI_LOADING, ENABLE_JENERETA_LOADING,
+    ENABLE_SENTIMENT_LOADING, DISABLE_AI_LOADING, DISABLE_JENERETA_LOADING, DISABLE_SENTIMENT_LOADING
+} from '../actions/constants';
 const defaultState: any = {
+    loadingSentiment: true,
+    loadingAI: true,
+    loadingJenereta: true,
     sentiment: '',
     ai: '',
     jenereta: '',
@@ -7,6 +13,42 @@ const defaultState: any = {
 
 export default function (state: any = defaultState, action: any) {
     switch (action.type) {
+        case ENABLE_SENTIMENT_LOADING: {
+            return {
+                ...state,
+                loadingSentiment: true,
+            }
+        }
+        case ENABLE_AI_LOADING: {
+            return {
+                ...state,
+                loadingAI: true,
+            }
+        }
+        case ENABLE_JENERETA_LOADING: {
+            return {
+                ...state,
+                loadingJenereta: true,
+            }
+        }
+        case DISABLE_AI_LOADING: {
+            return {
+                ...state,
+                loadingAI: false,
+            }
+        }
+        case DISABLE_JENERETA_LOADING: {
+            return {
+                ...state,
+                loadingJenereta: false,
+            }
+        }
+        case DISABLE_SENTIMENT_LOADING: {
+            return {
+                ...state,
+                loadingSentiment: false,
+            }
+        }
         case GET_AI_CODES: {
             return {
                 ...state,
